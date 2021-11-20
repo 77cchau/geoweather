530717d85f49c0ab398e8213ce4741f383c6b994
Carlos Delgadillo <carlosjdel02@gmail.com>
2021-11-20 15:40:24 -0800

    added comments

--- backend/weather.py
+++ backend/weather.py
@@ -6,11 +6,15 @@ import mapping
 
 WEATHER_URL_BASE = "https://api.weather.gov/points/"
 
+
+
 def build_weather_url(lat,lon):
     return f"{WEATHER_URL_BASE}{lat},{lon}"
 
 '''
 Uses link to give back the forecast data for a location
+    Uses a URL to give back the dataset associated with a particular location based on 
+    API data
 '''
 def build_weather_data(url):
     response = None
@@ -28,6 +32,8 @@ def build_weather_data(url):
 
 '''
 Uses data of a location to return a forecast API link
+    The forecast for a location in the API is in and of itself another link in the API, so we have
+    to load this url to get the forecast data
 '''
 def build_forecast_data(lat, lon):
     url = build_weather_url(lat,lon)
@@ -48,6 +54,11 @@ def build_forecast_data(lat, lon):
         if response != None:
             response.close()
 
+'''
+Returns a TUPLE of the most current and the next forecast of the day (in the API, this is
+the 0th and 1st index of an array called associated with the "periods" key in "properties")
+    Uses "Detailed Forecast" on the pro
+'''
 def get_todays_forecast(city,state):
     (lat,lon) = mapping.get_coords(city,state)
     return (build_forecast_data(lat,lon)["properties"]["periods"][0]["detailedForecast"],
