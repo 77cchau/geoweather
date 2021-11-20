229b23a1340d6556ec030041c2a8b3d53755826d
77cchau <77cchau@gmail.com>
2021-11-20 13:28:11 -0800

    renamed files

--- backend/weather.py
+++ backend/weather.py
@@ -1,10 +1,21 @@
 from flask import Flask
 from flask import request
-from flask_cors import CORS
+from flask import url_for
+
 
 app = Flask(__name__)
-CORS(app)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/verify", methods=["POST"])
+def verify():
+    if request.form["state"] != None and request.form["city"] != None:
+        return "200"
+    return "403"
+
+@app.route("/search", methods=["POST"])
+def search():
+    pass
+
